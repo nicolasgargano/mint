e2bca4d0df6763fdfc8832a69398942c2c335245
Nicolás Gargano <nicolasgargano@gmail.com>
2021-10-22 07:42:54 -0300

    feat: grid wireframe
    the normal wireframe draws all triangles, so I hand to implement a custom wireframe with line segments

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import { OrbitControls } from "@react-three/drei"
 import { Suspense } from "react"
 import { Canvas } from "@react-three/fiber"
-import { PlaneBufferGeometry } from "three"
+import {
+  BufferGeometry,
+  Float32BufferAttribute,
+  PlaneBufferGeometry,
+} from "three"
 import { noise } from "../util/noise"
 
 export const Home = () => {
@@ -29,9 +33,23 @@ const segments = 4
 const Terrain = () => {
   return (
     <group>
+      <mesh
+        position={[-1.2, 0, 0]}
+        geometry={terrainGeometry(segments, segments)}
+      >
+        <meshPhysicalMaterial wireframe />
+      </mesh>
+
       <mesh position={[0, 0, 0]} geometry={terrainGeometry(segments, segments)}>
         <meshPhysicalMaterial />
       </mesh>
+
+      <lineSegments
+        position={[1.2, 0, 0]}
+        geometry={gridGeometry(segments, segments)}
+      >
+        <lineBasicMaterial />
+      </lineSegments>
     </group>
   )
 }
@@ -50,6 +68,35 @@ const terrainGeometry = (widthSegments: number, depthSegments: number) => {
 
   geometry.computeTangents()
   geometry.computeVertexNormals()
+  return geometry
+}
+
+const gridGeometry = (widthSegments: number, depthSegments: number) => {
+  const stepX = 1 / widthSegments
+  const stepZ = 1 / depthSegments
+
+  const halfSize = 0.5
+  const vertices: number[] = []
+
+  for (let x = -halfSize; x <= halfSize; x += stepX) {
+    for (let z = -halfSize, countZ = 0; z <= halfSize; z += stepZ, countZ++) {
+      const y = terrainNoise(x, 0, z)
+      vertices.push(x, y, z)
+      if (countZ > 0 && countZ < depthSegments) vertices.push(x, y, z)
+    }
+  }
+
+  for (let z = -halfSize; z <= halfSize; z += stepZ) {
+    for (let x = -halfSize, countX = 0; x <= halfSize; x += stepX, countX++) {
+      const y = terrainNoise(x, 0, z)
+      vertices.push(x, y, z)
+      if (countX > 0 && countX < widthSegments) vertices.push(x, y, z)
+    }
+  }
+
+  const geometry = new BufferGeometry()
+  geometry.setAttribute("position", new Float32BufferAttribute(vertices, 3))
+
   return geometry
 }
 
