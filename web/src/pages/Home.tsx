e87186129cb1bb1d5a4dfb3abd05838e70711249
Nicolás Gargano <nicolasgargano@gmail.com>
2021-10-22 08:40:15 -0300

    feat: scene sketch

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,6 +1,15 @@
-import { Box, OrbitControls } from "@react-three/drei"
-import { Suspense } from "react"
-import { Canvas } from "@react-three/fiber"
+import {
+  Box,
+  Icosahedron,
+  OrbitControls,
+  Plane,
+  Tetrahedron,
+} from "@react-three/drei"
+import { Suspense, useLayoutEffect, useRef } from "react"
+import { Canvas, useThree } from "@react-three/fiber"
+import { DirectionalLight } from "three"
+import { useControls } from "leva"
+import { array, nonEmptyArray } from "fp-ts"
 
 export const Home = () => {
   return (
@@ -13,11 +22,75 @@ export const Home = () => {
 }
 
 const HomeScene = () => {
+  const directionalLightRef = useRef<DirectionalLight>()
+
+  const cameraControls = useControls("Camera Position", {
+    height: 1,
+    lookAt: -10,
+  })
+
+  const state = useThree()
+  useLayoutEffect(() => {
+    state.camera.position.set(0, cameraControls.height, 5)
+    state.camera.lookAt(0, 0, cameraControls.lookAt)
+  })
+
   return (
     <>
-      <ambientLight />
-      <Box />
-      <OrbitControls />
+      <directionalLight ref={directionalLightRef} position={[0, 4, 10]} />
+      {directionalLightRef.current && (
+        <directionalLightHelper args={[directionalLightRef.current]} />
+      )}
+      <ScrollingTerrain />
+      {/*<OrbitControls />*/}
     </>
+  )
+}
+
+const redish = 0xff4444
+
+const ScrollingTerrain = () => {
+  const planeControls = useControls("Plane Controls", {
+    size: 20,
+    divisions: { value: 24, min: 10, max: 100, step: 2 },
+  })
+
+  return (
+    <group>
+      <gridHelper
+        args={[planeControls.size, planeControls.divisions, redish, redish]}
+      />
+      <Plane
+        args={[50, 50]}
+        position={[0, -0.00001, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      >
+        <meshBasicMaterial color={"black"} />
+      </Plane>
+      {nonEmptyArray.range(0, 10).map((_, i) => (
+        <>
+          <Icosahedron scale={[1, 3, 1]} position={[-5, 0, (i - 5) * 2]}>
+            <meshPhysicalMaterial
+              metalness={1}
+              roughness={0}
+              color={0x440000}
+            />
+          </Icosahedron>
+          <Icosahedron scale={[1, 3, 1]} position={[-5, 0, (i - 5) * 2]}>
+            <meshBasicMaterial wireframe color={redish} />
+          </Icosahedron>
+          <Icosahedron scale={[1, 3, 1]} position={[5, 0, (i - 5) * 2]}>
+            <meshPhysicalMaterial
+              metalness={1}
+              roughness={0}
+              color={0x440000}
+            />
+          </Icosahedron>
+          <Icosahedron scale={[1, 3, 1]} position={[5, 0, (i - 5) * 2]}>
+            <meshBasicMaterial wireframe color={redish} />
+          </Icosahedron>
+        </>
+      ))}
+    </group>
   )
 }
