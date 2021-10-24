5ba9c799b2b012eb3fc99275ce56bfd8971bc989
Nicolás Gargano <nicolasgargano@gmail.com>
2021-10-24 10:28:11 -0300

    feat: preview noise with mesh and gradient textures

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { Route } from "wouter"
 import React from "react"
 import { Playground } from "./pages/Playground"
 import { Home } from "./pages/Home"
+import { Noise } from "./pages/Noise"
 
 export const App = () => {
   return (
@@ -12,6 +13,10 @@ export const App = () => {
 
       <Route path="/playground">
         <Playground />
+      </Route>
+
+      <Route path="/noise">
+        <Noise />
       </Route>
     </>
   )
